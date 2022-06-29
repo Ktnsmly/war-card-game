29764e6ff0b8a391561b1378af154600bd099771
Keaton <ktnsmly@gmail.com>
2022-06-29 12:12:11 -0700

    Initial Commit

---
+++ WarChallenge/WarChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarChallengeApp.swift
+//  WarChallenge
+//
+//  Created by Keaton Smiley on 6/16/22.
+//
+
+import SwiftUI
+
+@main
+struct WarChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
